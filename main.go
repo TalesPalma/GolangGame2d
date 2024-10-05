3319fd9621068ce8a3a749efd2ba056af5e4a6f7
TalesPalma <tales.palma_2022@outlook.com>
2024-10-04 21:28:09 -0300

    Moviments add

--- main.go
+++ main.go
@@ -1,20 +1,68 @@
 package main
 
 import (
+	"image"
+	"image/color"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Game struct{}
+type Game struct {
+	personagem *Personagem
+}
+
+type Personagem struct {
+	X      float64
+	Y      float64
+	assets *ebiten.Image
+}
 
 func (g *Game) Update() error {
+
+	keys := inpututil.AppendPressedKeys([]ebiten.Key{})
+	if len(keys) == 0 {
+		return nil
+	}
+
+	switch keys[0] {
+	case ebiten.KeyRight:
+		newImage, err := loadImage("assets/personagem02.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		g.personagem.X += 10
+		g.personagem.assets = newImage
+	case ebiten.KeyLeft:
+		newImage, err := loadImage("assets/personagem03.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		g.personagem.X -= 10
+		g.personagem.assets = newImage
+	default:
+		newImage, err := loadImage("assets/personagem01.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		g.personagem.assets = newImage
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.Black)
+	if g.personagem.assets != nil {
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(g.personagem.X, g.personagem.Y)
+		screen.DrawImage(g.personagem.assets, op)
+	}
+
+	ebitenutil.DebugPrint(screen, "Hello world")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -23,8 +71,37 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowTitle("Hello Tales")
+
+	image, err := loadImage("assets/personagem01.png")
+	if err != nil {
+		log.Fatal("Error ao carregar a imagem do personagem ", err)
+	}
+	game := &Game{
+		personagem: &Personagem{
+			X:      0,
+			Y:      0,
+			assets: image,
+		},
+	}
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func loadImage(path string) (*ebiten.Image, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ebiten.NewImageFromImage(img), nil
 }
